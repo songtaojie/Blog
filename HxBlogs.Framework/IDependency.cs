4c6e7710d1a81d65197a355f83d9919d719e6184
stjworkemail <stjworkemail@163.com>
2018-10-17 22:54:24 +0800

    修改框架

---
+++ HxBlogs.Framework/IDependency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HxBlogs.Framework
+{
+    /// <summary>
+    /// 每个依赖一个实例,使用每个依赖的作用域, 
+    /// 当你解析一个每个依赖一个实例的组件时, 你每次获得一个新的实例.
+    /// </summary>
+    public interface IDependency
+    {
+    }
+}
---
+++ HxBlogs.Framework/ISignleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HxBlogs.Framework
+{
+    /// <summary>
+    /// 创建单一实例的类
+    /// 也被称为 '单例.' 使用单一实例作用域,
+    /// 在根容器和所有嵌套作用域内所有的请求都将会返回同一个实例.
+    /// </summary>
+    public interface ISignleton
+    {
+    }
+}
