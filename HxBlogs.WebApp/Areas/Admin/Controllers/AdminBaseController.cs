99e604c2a0791f62910a2ff152fe24f134fd1dc1
stjworkemail <stjworkemail@163.com>
2018-10-29 23:16:56 +0800

    修改主页了列表

---
+++ HxBlogs.WebApp/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HxBlogs.WebApp.Areas.Admin
+{
+    [RouteArea("Admin")]
+    public class AdminBaseController:Controller
+    {
+    }
+}
