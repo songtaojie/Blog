3c390b9c37131718ee1615ce4ebc6e308c280afa
stjworkemail <stjworkemail@163.com>
2018-12-10 22:31:11 +0800

    修改项目结构

--- HxBlogs.WebApp/App_Start/BundleConfig.cs
+++ HxBlogs.WebApp/App_Start/BundleConfig.cs
@@ -10,41 +10,37 @@ namespace HxBlogs.WebApp
         {
             #region js文件
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/App/jquery.extendsion.js"));
+                        "~/Content/Scripts/jquery-{version}.js",
+                        "~/Content/App/js/jquery.extendsion.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/unobtrusive").Include(
-                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+                        "~/Content/Scripts/jquery.unobtrusive-ajax.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Content/Scripts/jquery.validate*"));
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
-                        "~/Scripts/App/scrollReveal.js"));
+                        "~/Content/App/js/scrollReveal.js"));
             bundles.Add(new ScriptBundle("~/bundles/metronic").Include(
-                        "~/Scripts/Metronic/app.js"));
+                        "~/Content/Plugin/Metronic/js/app.js"));
             // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Content/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Content/Scripts/bootstrap.js",
+                      "~/Content/Scripts/respond.js"));
             #endregion
 
             #region 样式文件
             bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
-                      "~/Content/bootstrap.css"));
+                      "~/Content/Css/bootstrap.css"));
             bundles.Add(new StyleBundle("~/Content/font").Include(
                      "~/Content/FontAwesome/css/font-awesome.css"));
 
             bundles.Add(new StyleBundle("~/Metronic/css").Include(
-                      "~/Content/Metronic/style/style-metro.css",
-                      "~/Content/Metronic/style/style.css",
-                      "~/Content/Metronic/style/style-responsive.css"));
-            bundles.Add(new StyleBundle("~/Content/app").Include(
-                      "~/Content/Css/index.css",
-                      "~/Content/Font/font.css"
-                      ));
+                      "~/Content/Plugin/Metronic/style/style-metro.css",
+                      "~/Content/Plugin/Metronic/style/style.css",
+                      "~/Content/Plugin/Metronic/style/style-responsive.css"));
             #endregion
         }
     }
---
+++ HxBlogs.WebApp/Controllers/PostEditController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HxBlogs.WebApp.Controllers
+{
+    public class PostEditController : Controller
+    {
+        // GET: PostEdit
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
