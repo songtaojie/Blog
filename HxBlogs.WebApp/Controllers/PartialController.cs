c8d4fec50bff8d9550192c92866a67b68e83d118
songtaojie <stjworkemail@163.com>
2019-06-04 19:18:14 +0800

    修改登录页

---
+++ HxBlogs.WebApp/Controllers/PartialController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HxBlogs.WebApp.Controllers
+{
+    public class PartialController : BaseNoAuthController
+    {
+        // GET: Partial
+        public ActionResult LoginPartial(string returnUrl)
+        {
+            if (!string.IsNullOrEmpty(returnUrl))
+            {
+                ViewBag.ReturnUrl = returnUrl;
+            }
+            return PartialView("LoginPartial");
+        }
+    }
+}
