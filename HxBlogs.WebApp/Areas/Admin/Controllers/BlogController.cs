d0af7896fc43b816e4925c79deca80ba0922d921
stjworkemail <stjworkemail@163.com>
2019-08-01 00:00:17 +0800

    修改后台管理

--- HxBlogs.WebApp/Areas/Admin/Controllers/BlogController.cs
+++ HxBlogs.WebApp/Areas/Admin/Controllers/BlogController.cs
@@ -18,8 +18,33 @@ namespace HxBlogs.WebApp.Areas.Admin.Controllers
         }
         public ActionResult Index()
         {
-           List<Blog> blogList =  this._blogService.GetEntitiesNoTrack(b => true, false).ToList();
-            return View(blogList);
+            return View();
+        }
+        public ActionResult LoadList()
+        {
+            int draw = Convert.ToInt32(Request["draw"]);
+            int start = Convert.ToInt32(Request["start"]);
+            int length = Convert.ToInt32(Request["length"]);
+            string search = Request["search"];
+            //没有过滤的记录数
+            long recordsTotal = this._blogService.LongCount(b => true, false);
+            if (string.IsNullOrEmpty(search)) {
+
+            }
+            var data = this._blogService.GetEntitiesNoTrack(b => true, false)
+                .OrderByDescending(b=>b.PublishDate)
+                .Skip(start * length)
+                .Take(length)
+                .ToList();
+            //过滤后的记录数
+            int recordsFiltered = data.Count;
+            return Json(new
+            {
+                draw,
+                recordsTotal,
+                recordsFiltered,
+                data
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
